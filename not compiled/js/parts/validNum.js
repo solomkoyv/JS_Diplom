2c1e229a01d9bf9d4d669fd0c6e0ccd6c2209380
Yura Solomko <solomkoyv@gmail.com>
2018-11-01 20:07:59 +0200

    Добавил не скомпилированные js

---
+++ not compiled/js/parts/validNum.js
@@ -0,0 +1,13 @@
+function validNum() {
+	const inputsPhone = document.querySelectorAll('input[name="user_phone"]'),
+		inputsSize = document.querySelectorAll('input[name="size"]');
+
+	function onlyNumber(input) {
+		input.onkeydown = function () {
+			return (this.value = this.value.replace(/[^0-9]/g, ""));
+		};
+	}
+	[...inputsPhone].forEach(elem => onlyNumber(elem));
+	[...inputsSize].forEach(elem => onlyNumber(elem));
+}
+export default validNum;
