7fc30651653c33aa881be60379eba7ec770f93fb
Yura Solomko <solomkoyv@gmail.com>
2018-11-01 16:36:24 +0200

    Начало Дипломной работы установлен webpack

---
+++ js/index.js
@@ -0,0 +1,22 @@
+// import tabs from './parts/tabs';
+// import modal from './parts/modal';
+// import timer from './parts/timer';
+// import forms from './parts/forms';
+// import slider from './parts/slider';
+// import calc from './parts/calc';
+// import events from './parts/events';
+import validNum from './parts/validNum';
+
+window.addEventListener("DOMContentLoaded", function () {
+
+	"use strict";
+
+	// tabs();
+	// modal();
+	// timer();
+	// forms();
+	// slider();
+	// calc();
+	// events();
+	validNum();
+});
---
+++ js/parts/validNum.js
@@ -0,0 +1,13 @@
+function validNum() {
+	const inputsPhone = document.querySelectorAll('input[name="user_phone"]'),
+		inputsCounter = document.querySelectorAll('.counter-block-input');
+
+	function onlyNumber(input) {
+		input.onkeydown = function () {
+			return (this.value = this.value.replace(/[^0-9]/g, ""));
+		};
+	}
+	[...inputsPhone].forEach(elem => onlyNumber(elem));
+	[...inputsCounter].forEach(elem => onlyNumber(elem));
+}
+export default validNum;
